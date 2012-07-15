fc50c0338c39428574af92bcb629b2b9f163662f
Mr Snow <mr.snow@houseoflaudanum.com>
2012-07-15 23:49:33 +1000

    Initial Vagrantfile.

---
+++ puppet/manifests/centos6.pp
@@ -0,0 +1,17 @@
+class centos6 {
+  user { "apache": 
+    ensure => "present", 
+  }
+  user { "puppet": 
+    ensure => "present", 
+  }
+  package { "git":
+    ensure => "present",
+  }
+  package { "wget":
+    ensure => "present",
+  }
+}
+
+
+include centos6
