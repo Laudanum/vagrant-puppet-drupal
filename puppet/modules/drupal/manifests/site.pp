1bffc49a55f80f5792fc5a555773ab91fcba69ff
Mr Snow <mr.snow@houseoflaudanum.com>
2012-08-16 11:12:30 +1000

    Trailing commas for drupal::site.

--- puppet/modules/drupal/manifests/site.pp
+++ puppet/modules/drupal/manifests/site.pp
@@ -4,50 +4,50 @@ define drupal::site(
   $drupal_root,
   $conf = { },
   $url = null,
-  $aliases = []
+  $aliases = [],
 ) {
 
   $primary_url = $url ? {
     null    => $name,
-    default => $url
+    default => $url,
   }
 
   $site = "${drupal_root}/sites/${primary_url}"
 
   # needed due to Puppet bug: http://projects.puppetlabs.com/issues/9322
   Exec {
-    path => "/bin:/sbin:/usr/bin:/usr/sbin"
+    path => "/bin:/sbin:/usr/bin:/usr/sbin",
   }
 
   exec { "${name}_drupal_root":
     command => "mkdir -p ${drupal_root}/sites",
-    creates => "${drupal_root}/sites"
+    creates => "${drupal_root}/sites",
   }
 
   file { "${name}_site_dir":
     ensure  => directory,
     path    => $site,
-    require => Exec["${name}_drupal_root"]
+    require => Exec["${name}_drupal_root"],
   }
 
   file { "${name}_site_files_dir":
     ensure  => directory,
     path    => "${site}/files",
-    require => File["${name}_site_dir"]
+    require => File["${name}_site_dir"],
     # todo: permissions
   }
 
   drupal::site_alias { $aliases:
     drupal_root => $drupal_root,
     site        => $primary_url,
-    require     => File["${name}_site_dir"]
+    require     => File["${name}_site_dir"],
   }
 
   file { "${name}_settings":
     ensure  => present,
     path    => "${site}/settings.php",
     content => template("drupal/settings.php.erb"),
-    require => File["${name}_site_dir"]
+    require => File["${name}_site_dir"],
   }
 
   apache::vhost { $primary_url:
@@ -55,8 +55,8 @@ define drupal::site(
     port          => 80,
     serveraliases => $aliases,
     serveradmin   =>  'admin@example.com',
-    logroot       => "/var/log/$apache::params::apache_name/${primary_url}"
-    override      => "All"
+    logroot       => "/var/log/$apache::params::apache_name/${primary_url}",
+    override      => "All",
   }
 
 }
