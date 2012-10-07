b072b50993bb7fe66fcfa0923d89b272ab87115e
Mr Snow <mr.snow@houseoflaudanum.com>
2012-10-08 07:11:42 +1100

    Add cnk.

--- puppet/manifests/laudanum-dev-box.pp
+++ puppet/manifests/laudanum-dev-box.pp
@@ -1,4 +1,4 @@
-$dev_domains = [ "spacetimeconcerto.com", "sturtassociates.com.au", "artlib.com.au", ] 
+$dev_domains = [ "spacetimeconcerto.com", "sturtassociates.com.au", "artlib.com.au", "cashmereandkaye.com", ] 
 
 define create_drupal_site {
 # apache::vhosts provides this
